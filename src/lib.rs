2ab5841180196e8fbeb304078af6a74d4421ef16
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-11-28 14:46:09 +0800

    Exporting functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,5 @@
-use astroxide::regions::{PointLocation, SphericalPolygon};
+use astroxide::regions::{PointLocation, SphericalAnulus, SphericalAperture, SphericalPolygon};
 use pyo3::prelude::*;
-
-// A test function
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
 
 #[pyclass]
 pub enum PointResult {
@@ -30,11 +24,47 @@ pub fn is_inside_polygon(
     }
 }
 
+#[pyfunction]
+pub fn is_inside_aperture(
+    ra_center: f64,
+    dec_center: f64,
+    radius_deg: f64,
+    ra_point: f64,
+    dec_point: f64,
+) -> PointResult {
+    let app = SphericalAperture::new(ra_center, dec_center, radius_deg);
+    let result = app.locate_point(ra_point, dec_point);
+    match result {
+        PointLocation::Inside => PointResult::Inside,
+        PointLocation::Outside => PointResult::Outside,
+        PointLocation::OnBoundary => PointResult::Edge,
+    }
+}
+
+#[pyfunction]
+pub fn is_inside_annulus(
+    ra_center: f64,
+    dec_center: f64,
+    inner_radius_deg: f64,
+    outer_radius_deg: f64,
+    ra_point: f64,
+    dec_point: f64,
+) -> PointResult {
+    let ann = SphericalAnulus::new(ra_center, dec_center, inner_radius_deg, outer_radius_deg);
+    let result = ann.locate_point(ra_point, dec_point);
+    match result {
+        PointLocation::Inside => PointResult::Inside,
+        PointLocation::Outside => PointResult::Outside,
+        PointLocation::OnBoundary => PointResult::Edge,
+    }
+}
+
 #[pymodule]
 fn regionx(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Add your functions here
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(is_inside_polygon, m)?)?;
+    m.add_function(wrap_pyfunction!(is_inside_annulus, m)?)?;
+    m.add_function(wrap_pyfunction!(is_inside_aperture, m)?)?;
     m.add_class::<PointResult>()?;
     Ok(())
 }
