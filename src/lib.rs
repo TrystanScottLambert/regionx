25521a51216aedcd86ed400bff306898c9c2159e
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-11-28 09:50:01 +0800

    Added new function

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,8 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
-#[pyclass] 
-enum PointResult {
+#[pyclass]
+pub enum PointResult {
     Inside,
     Outside,
     Edge,
@@ -16,11 +16,30 @@ enum PointResult {
 }
 
 #[pyfunction]
-fn is_inside_polygon(ra_verticies: Vec<f64>, dec_verticies, ra_int_point, dec_int_point, ra_point, dec_point)
+pub fn is_inside_polygon(
+    ra_verticies: Vec<f64>,
+    dec_verticies: Vec<f64>,
+    ra_int_point: f64,
+    dec_int_point: f64,
+    ra_point: f64,
+    dec_point: f64,
+) -> PointResult {
+    let poly =
+        SphericalPolygon::new(ra_verticies, dec_verticies, ra_int_point, dec_int_point).unwrap();
+    let result = poly.locate_point(ra_point, dec_point);
+    match result {
+        PointLocation::Inside => PointResult::Inside,
+        PointLocation::Outside => PointResult::Outside,
+        PointLocation::OnBoundary => PointResult::Edge,
+        PointLocation::Antipodal => PointResult::Antipodal,
+    }
+}
 
 #[pymodule]
 fn regionx(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Add your functions here
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(is_inside_polygon, m)?)?;
+    m.add_class::<PointResult>()?;
     Ok(())
 }
